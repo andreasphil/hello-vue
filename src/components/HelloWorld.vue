264c97fe13442249f4508c99b316d2c0e02fc513
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-02-13 15:24:37 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-counter v-model="count" :goal="goal" @done="showHelloWorld"/>
+    <div class="hello-world" v-if="goalMet">Hello Vuerld 🙋🏻‍♀️</div>
+  </div>
+</template>
+
+<script>
+import VCounter from "@/components/VCounter.vue";
+
+export default {
+  components: {
+    VCounter
+  },
+
+  data() {
+    return {
+      count: 0,
+      goal: 10,
+      goalMet: false
+    };
+  },
+
+  methods: {
+    showHelloWorld() {
+      this.goalMet = true;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.hello-world {
+  color: #FFF;
+  font-size: 60px;
+  line-height: 1.1;
+  margin-top: 50px;
+}
+</style>
---
+++ src/components/VCount.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- The template doesn't care what the source of the data is as long as it's
+  somehwere inside the component's scope-->
+  <div class="count">{{ displayCount }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      required: true
+    }
+  },
+
+  computed: {
+    displayCount() {
+      // Vue automatically figures out which props/data the computed prop depends on
+      // and recalculates it accordingly
+      return this.count || "😴";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.count {
+  color: #FFF;
+  font-size: 100px;
+  font-weight: bold;
+  line-height: 1.1;
+  margin-bottom: 50px;
+  text-align: center;
+}
+</style>
